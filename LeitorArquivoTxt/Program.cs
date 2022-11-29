74b66df4aea4e45f9a1af8a2b5c0020cf1be9bec
Elisandro <Elisandro@LAPTOP-CCH60LK6>
2022-11-29 02:20:22 -0300

    Adicionar arquivos de projeto.

---
+++ LeitorArquivoTxt/Program.cs
@@ -0,0 +1,27 @@
+﻿class program
+{
+    static void Main()
+    {
+        //Código para leitura por linha
+
+        const string filePath = "c:\\code\\txt.txt";
+
+        using var file = new StreamReader(filePath);
+        string? line;
+
+        while ((line = file.ReadLine()) != null)
+            Console.WriteLine(line);
+
+
+
+        //Código para leitura texto 
+        
+        /*
+        const string filePath = "c:\\code\\txt.txt";
+
+        var data = File.ReadAllText(filePath);
+
+        Console.WriteLine(data);
+        */
+    }
+}
